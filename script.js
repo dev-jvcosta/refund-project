c75ee46b3fc1be5b89f5299100466a20c3ac603e
devjohnes <dev.jvcosta@gmail.com>
2025-10-04 17:26:18 -0300

    Add initial HTML structure and JavaScript for expense request form

---
+++ script.js
@@ -0,0 +1,10 @@
+// Seleciona os elementos do formuláro.
+const amount = document.getElementById("amount");
+
+/**
+ * Evento: .oninput()
+ * Dispara um evento que captura toda informação inserida no input amount, inclusive quando apaga o valor inserioæ
+ */
+amount.oninput = () => {
+  
+};
