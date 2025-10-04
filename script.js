1002e959c1ecae8dad0e612b260d384a1c937183
devjohnes <dev.jvcosta@gmail.com>
2025-10-04 20:52:58 -0300

    Enhance input validation for amount field by adding mask to validate the input information

--- script.js
+++ script.js
@@ -6,5 +6,13 @@ const amount = document.getElementById("amount");
  * Dispara um evento que captura toda informação inserida no input amount, inclusive quando apaga o valor inserioæ
  */
 amount.oninput = () => {
-  
+  /**
+   *  / \D /g, ""
+   *  \D = Remove tudo que não for número
+   *  g = Global, ou seja, remove todos os caracteres que não forem números
+   *  "" = Substitui por nada, ou seja, remove os caracteres que não forem números
+   *  value = Pega o valor do input amount e aplica a máscara
+   */
+  let value = amount.value.replace(/\D/g, ""); 
+  amount.value = value; // Atualiza o valor do input amount com a máscara aplicada
 };
