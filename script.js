94bcee9831d5683e901bd5d46fa2bfce652ac476
devjohnes <dev.jvcosta@gmail.com>
2025-10-04 22:37:16 -0300

    Update amount input placeholder and enhance currency formatting in script.js

--- script.js
+++ script.js
@@ -5,14 +5,28 @@ const amount = document.getElementById("amount");
  * Evento: .oninput()
  * Dispara um evento que captura toda informação inserida no input amount, inclusive quando apaga o valor inserioæ
  */
+
+// Captura o evento de input para formatar o valor
 amount.oninput = () => {
-  /**
-   *  / \D /g, ""
-   *  \D = Remove tudo que não for número
-   *  g = Global, ou seja, remove todos os caracteres que não forem números
-   *  "" = Substitui por nada, ou seja, remove os caracteres que não forem números
-   *  value = Pega o valor do input amount e aplica a máscara
-   */
-  let value = amount.value.replace(/\D/g, ""); 
-  amount.value = value; // Atualiza o valor do input amount com a máscara aplicada
+  
+  // Obtém o valor atual do input e remove os caracteres não numéricos
+  let value = amount.value.replace(/\D/g, "");
+
+  // transformar o valor em centavos (ex.: 150 / 100 = 1.5 que é igual a R$1,50)
+  value = Number(value) / 100;
+
+  // Atualiza o valor do input com o valor formatado
+  amount.value = formatCurrencyBRL(value); 
+
+ 
 };
+
+function formatCurrencyBRL(value){
+  value = value.toLocaleString("pt-BR", {
+    //Formata o valor no padrão brasileiro
+    style: "currency", //mooeda
+    currency: "BRL" // formato da moeda
+  });
+  //retorna o valor formatado
+  return value;
+}
